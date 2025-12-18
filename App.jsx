dc0f4148b482491500ede6f82bd7563079f53198
AbdulAleemWorks <work.abdulaleemfaisal@gmail.com>
2025-12-18 19:03:29 +0500

    First Commit !

---
+++ App.jsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RootNavigator } from './app/navigation/navigation';
+import "./global.css";
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle="dark-content" />
+      <RootNavigator />
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
